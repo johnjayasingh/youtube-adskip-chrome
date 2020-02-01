21df7aa1cca5ab67cbd7fc5c8b488dde3271742d
john <john>
2020-02-01 12:23:36 +0530

    optimum

--- adskip.js
+++ adskip.js
@@ -1,35 +1,27 @@
 setInterval(() => {
     const adSkip = document.getElementsByClassName('ytp-ad-skip-button');
-
-    var overlayAds = document.getElementsByClassName('ytp-ad-overlay-container');
-    const playerAds = document.getElementById('player-ads')
-    const googleCompanion = document.getElementById('google_companion_ad_div');
-
-    const homeAd = document.getElementById('masthead-ad');
-    const offer = document.getElementById('offer-module');
-    const movieOffer = document.getElementById('ytd-movie-offer-module-renderer');
+    const overlayAds = document.getElementsByClassName('ytp-ad-overlay-container');
     const ads = document.getElementsByClassName('adDisplay');
     const googleView = document.getElementsByClassName('GoogleActiveViewClass');
 
-    for (let i = 0; i < adSkip.length; i++) {
-        adSkip[i].click();
-        const info = document.getElementById('info-skeleton');
-        info.innerHTML = '';
-    }
+    const playerAds = document.getElementById('player-ads')
+    const googleCompanion = document.getElementById('google_companion_ad_div');
+    const homeAd = document.getElementById('masthead-ad');
+    const offer = document.getElementById('offer-module');
+    const movieOffer = document.getElementById('ytd-movie-offer-module-renderer');
 
-    for (let i = 0; i < ads.length; i++) {
-        ads[i].style.display = "none";
-        const info = document.getElementById('info-skeleton');
-        info.innerHTML = '';
-    }
+    const iterations = Math.max([adSkip.length, overlayAds.length, ads.length, googleView.length])
 
-    for (var i = 0; i < overlayAds.length; i++) {
-        overlayAds[i].innerHTML = "";
-    }
-
-
-    for (let i = 0; i < googleView.length; i++) {
-        googleView[i].style.display = "none";
+    for (let i = 0; i < iterations; i++) {
+        if (adSkip[i]) {
+            adSkip[i].click();
+        }
+        if (ads[i]) {
+            ads[i].style.display = "none";
+        }
+        if (googleView[i]) {
+            googleView[i].style.display = "none";
+        }
     }
 
     if (playerAds) {
